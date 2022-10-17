2530d0bbe19e0031d9dec04d578a87d21d990117
Pedro Simao <pedrosimao@gmail.com>
2022-10-17 15:47:25 +0100

    removed motion

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,23 @@
-import { useAnimation, motion, animate } from "framer-motion"
-
-const buttonMotion = {
-  rest: {
-    opacity: 0
-  },
-  hover: {
-    opacity: 1,
-    transition: {
-      duration: 2,
-      ease: 'easeOut'
-    }
-  }
-}
-
-const divMotion = {
-  rest: {
-    opacity: 1
-  },
-  hover: {
-    width: '60%',
-    transition: {
-      duration: 2,
-      ease: 'easeOut'
-    }
-  }
-}
 
 export default function Home() {
   return (
     <>
       <div 
         className='flex flex-wrap h-screen bg-teal-500'>
-          <div className="flex flex-grow justify-center items-center h-screen bg-red-500 border-2 hover:w-[50%]">
-            <p className="px-5 py-1 border-2 mx-3">Botão 1</p>
+          <div className="flex flex-grow justify-center items-center h-screen
+           br-slate-800 group hover:w-[50%] transition-[width] duration-500 ease-in-out
+           bg-fixed bg-cover bg-left bg-column1 bg-no-repeat">
+            <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 1</p>
           </div>
-          <div className="flex flex-grow justify-center items-center h-screen bg-red-500 border-2 hover:w-[50%]">
-            <p className="px-5 py-1 border-2 mx-3">Botão 2</p>
+          <div className="flex flex-grow justify-center items-center h-screen
+           bg-red-500 group hover:w-[50%]
+           bg-fixed bg-cover bg-center bg-column2 bg-no-repeat">
+            <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 2</p>
           </div>
-          <div className="flex flex-grow justify-center items-center h-screen bg-red-500 border-2 hover:w-[50%]">
-            <p className="px-5 py-1 border-2 mx-3">Botão 3</p>
+          <div className="flex flex-grow justify-center items-center h-screen 
+          bg-red-500 group hover:w-[50%]
+          bg-fixed bg-cover bg-right bg-column3 bg-no-repeat">
+            <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 3</p>
           </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      transitionProperty: {
+        width: 'width',
+      },
+      backgroundImage: {
+        'column1': "url('/images/media-1.jpg')",
+        'column2': "url('/images/media-2.jpg')",
+        'column3': "url('/images/media-3.jpg')",
+      }
     },
   },
   plugins: [],
