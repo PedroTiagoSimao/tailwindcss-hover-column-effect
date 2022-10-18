49cdef9992d7468e68b3c90799dfbbc1f5271280
Pedro Simao <pedrosimao@gmail.com>
2022-10-18 08:07:36 +0100

    code refactor by Fernando!

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,31 @@
-import { useState } from "react"
+import { userAgent } from "next/server"
+import { useState, useRef, useEffect, Children } from "react"
 
-export default function Home() {
+export default function Home({ children }) {
 
-  const [col1Active, setCol1Active] = useState('w-[33.33%]')
-  const [col2Active, setCol2Active] = useState('w-[33.33%]')
-  const [col3Active, setCol3Active] = useState('w-[33.33%]')
-
-  console.log(col1Active);
+  const [colActive, setColActive] = useState(null)
 
   return (
     <>
       <div 
-        className='flex flex-wrap h-screen bg-teal-500'>
-          <div className={`flex flex-grow justify-center items-center h-screen br-slate-800 group z-0 ${col1Active} transition-[width] duration-500 ease-in-outbg-fixed bg-cover bg-left bg-column1 bg-no-repeat`}
-           onMouseEnter={() => (setCol1Active('w-[50%]'), setCol2Active('w-[12.5%]'), setCol3Active('w-[12.5%]') )} 
-           onMouseLeave={() => (setCol1Active('w-[33.33%]'), setCol2Active('w-[33.33%]'), setCol3Active('w-[33.33%]') )} 
-           >
+        className='flex flex-wrap h-screen'>
+          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col1' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 ease-in-outbg-fixed bg-cover bg-center bg-column1 bg-no-repeat`}
+          onMouseEnter={() => setColActive('col1')} 
+          onMouseLeave={() => setColActive(null)} 
+          >
             <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 1</p>
           </div>
-          <div className={`flex flex-grow justify-center items-center h-screen bg-red-500 group z-0 ${col2Active} transition-[width] duration-500 ease-in-out bg-fixed bg-cover bg-center bg-column2 bg-no-repeat`}
-           onMouseEnter={() => (setCol1Active('w-[12.5%]'), setCol2Active('w-[50%]'), setCol3Active('w-[12.5%]') )} 
-           onMouseLeave={() => (setCol1Active('w-[33.33%]'), setCol2Active('w-[33.33%]'), setCol3Active('w-[33.33%]') )} 
-           >
-            <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 2</p>
-          </div>
-          <div className={`flex flex-grow justify-center items-center h-screen bg-red-500 group z-0 ${col3Active} transition-[width] duration-500 ease-in-out bg-fixed bg-cover bg-right bg-column3 bg-no-repeat`}
-          onMouseEnter={() => (setCol1Active('w-[12.5%]'), setCol2Active('w-[12.5%]'), setCol3Active('w-[50%]') )} 
-           onMouseLeave={() => (setCol1Active('w-[33.33%]'), setCol2Active('w-[33.33%]'), setCol3Active('w-[33.33%]') )} 
+          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col2' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 ease-in-outbg-fixed bg-cover bg-center bg-column2 bg-no-repeat`}
+          onMouseEnter={() => setColActive('col2')} 
+          onMouseLeave={() => setColActive(null)} 
           >
-            <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 3</p>
+            <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 1</p>
+          </div>
+          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col3' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 ease-in-outbg-fixed bg-cover bg-center bg-column3 bg-no-repeat`}
+          onMouseEnter={() => setColActive('col3')} 
+          onMouseLeave={() => setColActive(null)} 
+          >
+            <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 1</p>
           </div>
       </div>
     </>
