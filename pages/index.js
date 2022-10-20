0e605dbe9c4e16c2739de97771c8384e8d143374
Pedro Simao <pedrosimao@gmail.com>
2022-10-20 21:06:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,19 @@ export default function Home({ children }) {
     <>
       <div 
         className='flex flex-wrap h-screen'>
-          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col1' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 ease-in-outbg-fixed bg-cover bg-center bg-column1 bg-no-repeat`}
+          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col1' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 delay-300 ease-in-out bg-fixed bg-cover bg-center bg-column1 bg-no-repeat`}
           onMouseEnter={() => setColActive('col1')} 
           onMouseLeave={() => setColActive(null)} 
           >
             <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 1</p>
           </div>
-          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col2' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 ease-in-outbg-fixed bg-cover bg-center bg-column2 bg-no-repeat`}
+          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col2' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 delay-300 ease-in-out bg-fixed bg-cover bg-center bg-column2 bg-no-repeat`}
           onMouseEnter={() => setColActive('col2')} 
           onMouseLeave={() => setColActive(null)} 
           >
             <p className="px-5 py-1 border-[1px] mx-3 opacity-0 group-hover:opacity-100 transition ease-in-out duration-500">Botão 1</p>
           </div>
-          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col3' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 ease-in-outbg-fixed bg-cover bg-center bg-column3 bg-no-repeat`}
+          <div className={`flex flex-grow justify-center items-center h-screen group z-0 ${colActive ? colActive === 'col3' ? 'w-[50%]' : 'w-[12.5%]' : 'w-[33.33%]'} transition-[width] duration-1000 delay-300 ease-in-out bg-fixed bg-cover bg-center bg-column3 bg-no-repeat`}
           onMouseEnter={() => setColActive('col3')} 
           onMouseLeave={() => setColActive(null)} 
           >
